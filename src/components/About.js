682d17c1f7b6bf0fd2ae24398efda69003809bdc
ingmarhendrik <68082792+ingmarhendrik@users.noreply.github.com>
2024-10-15 16:16:35 +0300

    Update projects page

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,21 @@ import '../styles/About.css';
 const About = () => {
     return (
         <section id="about">
-            <h1>ABOUT ME</h1>
             <div className="about-container">
-                <p className="regular-text">
-                    My name is Ingmar Hendrik Rohusaar. I am currently a software development student in my final 
-                    year at Tallinn Centre of Industrial Education (Tallinna Tööstushariduskeskus). 
-                    I have built a solid foundation in software development, particulalry in areas such as JavaScript,
-                    C#, ASP.NET, Python, SQL and Git and I enjoy applying agile development methods in collaborative 
-                    environments. I actively seek out opportunities to expand my knowledge and skills, embracing new
-                    technologies and approaches.
-                </p>
-                <img src={aboutImage} alt="Hi. This is me." />
+                <div className="about-text">
+                    <h1>ABOUT ME</h1>
+                    <p className="regular-text">
+                        My name is Ingmar Hendrik Rohusaar. I am currently a software development student in my final 
+                        year at Tallinn Centre of Industrial Education (Tallinna Tööstushariduskeskus). 
+                        I have built a solid foundation in software development, particulalry in areas such as JavaScript, React,
+                        C#, ASP.NET, SQL and Git and I enjoy applying agile development methods in collaborative 
+                        environments. I actively seek out opportunities to expand my knowledge and skills, embracing new
+                        technologies and approaches.
+                    </p>
+                </div>
+                <div className="about-image">
+                    <img src={aboutImage} alt="Hi. This is me." />
+                </div>
             </div>
         </section>
     );
